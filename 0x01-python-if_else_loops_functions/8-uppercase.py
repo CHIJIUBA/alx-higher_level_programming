67c07ebe330a64b5d7b9571d58c0646f39fb416a
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-06-28 07:03:27 -0700

    ninth

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def to_upper(character):
     if ord(character) >= 97 and ord(character) <= 122:
-        return (ord(character) -32)
+        return (ord(character) - 32)
     else:
         return ord(character)
+
 
 def uppercase(string):
     string_new = ""
