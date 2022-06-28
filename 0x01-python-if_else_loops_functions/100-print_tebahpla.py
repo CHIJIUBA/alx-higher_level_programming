4d09a94c3e5c654d61e6496d7edf73e8f2ba6f0a
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-06-28 16:18:46 +0100

    go

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 != 0:
-        i = i - 32
-    print("{}".format(chr(i)), end="")
+for ascii_num in range(122, 96, -1):
+	if ascii_num % 2 == 1:
+		ascii_num = ascii_num - 32
+	print("{:c}".format(ascii_num), end='')
