b836e41536d46229658fc39f48a03be3cb86755c
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-07-03 20:03:15 +0100

    go

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""function that removes all characters c and C from a string"""
+
+
+def no_c(my_string):
+    new_string = ""
+    count = 0
+    for i in my_string:
+        if not ((i == 'c') or (i == 'C')):
+            new_string = new_string + i
+    return new_string
