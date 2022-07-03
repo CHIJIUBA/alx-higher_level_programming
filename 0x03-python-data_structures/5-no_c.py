2ec88970bdca2f4428bbce11837a525368a49e91
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-07-03 20:07:34 +0100

    go

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,5 +6,5 @@ def no_c(my_string):
     new_string = ""
     for i in my_string:
         if not ((i == 'c') or (i == 'C')):
-            new_string = new_string + i
+            new_string = new_string + "{:s}".format(i)
     return new_string
