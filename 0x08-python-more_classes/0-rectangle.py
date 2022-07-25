f58ebc9dfd4106a5d38865e6995750ee6811cf05
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-07-25 23:46:43 +0100

    first

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+class Rectangle:
+    """Define a class Rectangle"""
+
+    def __init__(self):
+        """Initialize a new rectangle"""
+        pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+Python more classes
+
+Chijiuba Victroy
