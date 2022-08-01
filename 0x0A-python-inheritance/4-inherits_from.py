e42b2b0dfc1c682609eec69459b40ca403563d9c
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-08-02 00:22:43 +0100

    go

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a class-checking function."""
+
+
+def inherits_from(obj, a_class):
+    """Check if an object is inherits from a given class.
+
+    Args:
+        obj (any): The object to check.
+        a_class (type): The class to match the type of obj to.
+    Returns:
+        If obj inherits from a_class - True.
+        Otherwise - False.
+    """
+    if issubclass(obj, a_class):
+        return True
+    return False
