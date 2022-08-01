2d358f7fbaee4086be303a44a4d620d6e9adf84b
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-08-02 00:55:08 +0100

    go

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Defines a BaseGeometry class"""
+"""Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
-    """Defines a BaseGeometry class"""
+    """Reprsent base geometry."""
+
     def area(self):
+        """Not yet implemented."""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
