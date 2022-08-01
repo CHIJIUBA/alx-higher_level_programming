2792afe15cf0673ee4773abf5682825b9cdaf38f
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-08-02 00:37:06 +0100

    go

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Defines a BaseGeometry class"""
+    def area(self):
+        raise Exception("area() is not implemented")
