ff8c3921303a3cd1aea8cbe4f679316c5259b4ba
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-08-03 00:04:52 +0100

    go

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Writes an Object to a text file, using a JSON representation"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Writes an Object to a text file, using a JSON representation"""
+    text = json.dumps(my_obj)
+    with open(filename, mode="w", encoding="utf_8") as f:
+        f.write(text)
