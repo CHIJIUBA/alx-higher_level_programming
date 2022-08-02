47d6bf60474ecdf47178788b4d73c4351cedd47e
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-08-02 23:05:19 +0100

    go

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
         filename (any): The name of the file to read.
     """
     with open(filename, encoding="utf-8") as myFile:
-        print(myFile.read())
+        print(myFile.read(), end="")
