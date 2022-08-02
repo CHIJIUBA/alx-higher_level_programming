be5efcc0ff323ba195dc74e4b2985ae3d8b16ddc
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-08-02 10:15:53 +0100

    go

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -9,6 +9,9 @@ class MyList(list):
     """ Public instance method: def print_sorted(self):
     that prints the list, but sorted (ascending sort)
     """
+    def __init__(self):
+        super().__init__
+
     def print_sorted(self):
         """print sorted list"""
         print(sorted(self))
