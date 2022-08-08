3fc0fad5f4a7e4238047a8d182723d4b7d54dbd7
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-08-09 00:51:08 +0100

    go

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,17 +17,6 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
-    @property
-    def size(self):
-        """Get/set the size of the Square."""
-        return self.width
-
-    @size.setter
-    def size(self, value):
-        """Get/set the size of the Square."""
-        self.width = value
-        self.height = value
-
     def __str__(self):
         """Returns the str() and print() rep of the class"""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.size, self.size)
