192d5c1727ee9b6305ebfdba1d26f3f3cf20c17c
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-08-08 23:20:42 +0100

    go

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a Base class."""
+
+
+class Base:
+    """Represent a Base Class.
+
+    Attributes:
+        __nb_objects (int): The number of object instances.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize a new Base Object.
+
+        Args:
+            id (int): The id of the new base object.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            self.__nb_objects += 1
+            self.id = self.__nb_objects
