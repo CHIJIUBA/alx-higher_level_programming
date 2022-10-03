ea3da692327425372da155d73af152bd893953b5
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-10-03 23:51:08 +0100

    go

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,2 @@
 #!/usr/bin/node
-const count = process.argv.length;
-console.log(count <= 2 ? 'No argument' : process.argv[2]);
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
