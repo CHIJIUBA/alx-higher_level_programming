faca1b89c1d3ff048c4b1ec8f140484f97ee4466
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-10-09 02:42:19 +0100

    go

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+List all the cities in the db hbtn_0e_0_usa
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(db=argv[3], user=argv[1], passwd=argv[2])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM `cities` ORDER BY `id` ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
