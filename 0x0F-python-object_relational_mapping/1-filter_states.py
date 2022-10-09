54b69bc09ba87c0f70899cfd281a60ebfc71276a
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-10-09 02:04:19 +0100

    go

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- lists all states with a name starting with N (upper N) 
+ lists all states with a name starting with N (upper N)
  from the db hbtn_0e_0_usa
 """
 import MySQLdb
