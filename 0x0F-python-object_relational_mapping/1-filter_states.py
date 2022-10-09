db55e8d3ce7a3fb25e02807f0755d81568c17481
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-10-09 02:02:04 +0100

    go

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+ lists all states with a name starting with N (upper N) 
+ from the db hbtn_0e_0_usa
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(db=argv[3], user=argv[1], passwd=argv[2])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
