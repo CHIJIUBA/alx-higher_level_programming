d0c5ee9099c9f316cbbb6de099a6695c9bae3f2d
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-10-10 01:16:48 +0100

    go

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    rows = session.query(City, State).filter(City.state_id == State.id)\
+    rows = session.query(City, State).join(City.state_id, State.id)\
                                      .order_by(City.id).all()
     for city, state in rows:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
