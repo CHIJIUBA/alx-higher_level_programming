da7202103fa61947ad3ca4738ac9495e67bb1623
Chijiuba Victory <chijiubaonyedikachukwu@gmail.com>
2022-11-17 07:47:19 +0100

    go

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
 -- Creates the user user_0d_1 with all privileges.
 CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
+    IF NOT EXISTS 'chijiuba'@'localhost'
+    IDENTIFIED BY 'chijiuba';
 GRANT ALL PRIVILEGES
    ON *.*
-   TO 'user_0d_1'@'localhost';
+   TO 'chijiuba'@'localhost';
